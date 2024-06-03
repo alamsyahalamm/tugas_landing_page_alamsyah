5f31f88ad09efe0eaff81e894bc5617b1cee3bab
alamsyahalamm <muhammad728744@smk.belajar.id>
2024-06-03 16:26:17 +0700

    Adding cards

--- src/components/contents.js
+++ src/components/contents.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from "flowbite-react";
+import { Button, Card } from "flowbite-react";
 
 function Contents() {
     return (
@@ -21,6 +21,67 @@ function Contents() {
                         <source src="mov_bbb.ogg" type="video/ogg" />
                         Your browser does not support HTML video.
                     </video>
+                </div>
+            </div>
+            <div id="news">
+                <div className="grid lg:grid-cols-3 lg:gap-12 gap-4 mx-6">
+                    <div class=" bg-white rounded-lg dark:bg-gray-800 text-justify">
+                        <div className="py-2">
+                            <a href="#">
+                                <h5 class="mb-2 text-[23px] font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                            </a>
+                        </div>
+                        <a href="#">
+                            <img class="rounded-lg w-full max-h-60 object-cover object-top" src="/src/img/1.jpg" alt="" />
+                        </a>
+                        <div class="p-5 px-0 text-justify">
+                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                Selengkapnya
+                                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                    <div class=" bg-white rounded-lg dark:bg-gray-800 text-justify">
+                        <div className="py-2">
+                            <a href="#">
+                                <h5 class="mb-2 text-[23px] font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                            </a>
+                        </div>
+                        <a href="#">
+                            <img class="rounded-lg w-full max-h-60 object-cover object-top" src="/src/img/2.jpg" alt="" />
+                        </a>
+                        <div class="p-5 px-0 text-justify">
+                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                Selengkapnya
+                                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                    <div class=" bg-white rounded-lg dark:bg-gray-800 text-justify">
+                        <div className="py-2">
+                            <a href="#">
+                                <h5 class="mb-2 text-[23px] font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                            </a>
+                        </div>
+                        <a href="#">
+                            <img class="rounded-lg w-full max-h-60 object-cover object-top" src="/src/img/3.jpg" alt="" />
+                        </a>
+                        <div class="p-5 px-0 text-justify">
+                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                Selengkapnya
+                                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
